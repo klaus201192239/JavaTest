8ca8ab173f6754248fa11c80620080ec80638bb4
klaus20192239 <13898624331@163.com>
2016-04-25 20:26:47 +0800

    First Submit
    Java Test Application is built!

---
+++ src/main/java/com/klaus/main/TestMain.java
@@ -0,0 +1,12 @@
+package com.klaus.main;
+
+public class TestMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("hahTest");
+
+	}
+
+}
